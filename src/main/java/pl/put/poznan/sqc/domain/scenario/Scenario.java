ef585b06b4ddad33fe3cfcd3a0a4edaebb22a0b6
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2021-12-12 21:34:45 +0100

    Update Scenario.java
    Co-Authored-By: Kacper Banasik <kacper.banasik13b@gmail.com>
    Co-Authored-By: wojhok <57602697+wojhok@users.noreply.github.com>

--- src/main/java/pl/put/poznan/sqc/domain/scenario/Scenario.java
+++ src/main/java/pl/put/poznan/sqc/domain/scenario/Scenario.java
@@ -3,6 +3,7 @@ package pl.put.poznan.sqc.domain.scenario;
 import pl.put.poznan.sqc.domain.visitors.Visitor;
 
 // TODO: 2021-12-10
+//  the whole class
 public class Scenario implements Visitable {
     @Override
     public void
