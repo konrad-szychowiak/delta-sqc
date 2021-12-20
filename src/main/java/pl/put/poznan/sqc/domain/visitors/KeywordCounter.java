70a5d6563c5d524b891d0d3c131e09257f79a24e
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2021-12-20 12:12:01 +0100

    Polish KeywordCounter's documentation
    Co-Authored-By: JAuguscik <92777170+JAuguscik@users.noreply.github.com>

--- src/main/java/pl/put/poznan/sqc/domain/visitors/KeywordCounter.java
+++ src/main/java/pl/put/poznan/sqc/domain/visitors/KeywordCounter.java
@@ -1,41 +1,45 @@
 package pl.put.poznan.sqc.domain.visitors;
 
+import pl.put.poznan.sqc.domain.SQCService;
 import pl.put.poznan.sqc.domain.scenario.Step;
+
 /**
- *Concrete class of CounterVisitor
- *<p>Increment the counter for each keyword encountered</p>
- *<p>The keywords are:
- *<b>IF</b>
- *<b>ELSE</b>
- *<b>FOR EACH</b></p>
+ * Concrete class of CounterVisitor
+ * <p>Increment the counter for each keyword encountered in Scenario's Steps</p>
+ * <p>The keywords are:
+ * <b>IF</b>,
+ * <b>ELSE</b>,
+ * <b>FOR EACH</b>.</p>
+ *
  * @see CounterVisitor
  * @see Visitor
+ * @see pl.put.poznan.sqc.domain.scenario.Scenario
+ * @see Step
+ * @see SQCService#getKeywordCount()
  */
-
 public class KeywordCounter extends CounterVisitor {
-    @Override
-    public void
-    /** Increment the counter if one of keywords is encountered
-     * Checks whether the text of step starts with any of keywords and a space
-     * If encounters exception due to too small text returns nothing: no keywords in text
+
+    /**
+     * Increment the counter if one of keywords is encountered.
+     *
+     * <p>Checks whether the text of step starts with any of keywords and a space.</p>
+     *
      * @see CounterVisitor
      */
+    @Override
+    public void
     visit(Step step) {
-
         String temp_text = step.getText();
 
-        try{
-            if(temp_text.substring(0, 3).equals("IF ")){
-                this.incrementCount();
+        if (temp_text.startsWith("IF ")) {
+            this.incrementCount();
 
-            }else if(temp_text.substring(0, 5).equals("ELSE ")){
-                this.incrementCount();
+        } else if (temp_text.startsWith("ELSE ")) {
+            this.incrementCount();
 
-        }else if(temp_text.substring(0, 9).equals("FOR EACH ")){
-                this.incrementCount();
-            }
-        } catch (StringIndexOutOfBoundsException e){
-            return;
+        } else if (temp_text.startsWith("FOR EACH ")) {
+            this.incrementCount();
         }
+
     }
 }
