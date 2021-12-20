78cdaabf2cdc656cf12fe60c8761062d7e18b30c
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2021-12-20 11:30:15 +0100

    Document StepCounter

--- src/main/java/pl/put/poznan/sqc/domain/visitors/StepCounter.java
+++ src/main/java/pl/put/poznan/sqc/domain/visitors/StepCounter.java
@@ -1,8 +1,22 @@
 package pl.put.poznan.sqc.domain.visitors;
 
+import pl.put.poznan.sqc.domain.SQCService;
+import pl.put.poznan.sqc.domain.scenario.Scenario;
 import pl.put.poznan.sqc.domain.scenario.Step;
 
+
 public class StepCounter extends CounterVisitor {
+    /**
+     * Visit a step in a scenario.
+     *
+     * <p>Increment the counter for each Step encountered in a Scenario,
+     * no matter the step's content.</p>
+     *
+     * @param step Step which will be analysed
+     * @see Step
+     * @see Scenario
+     * @see SQCService#getStepCount()
+     */
     @Override
     public void
     visit(Step step) {
